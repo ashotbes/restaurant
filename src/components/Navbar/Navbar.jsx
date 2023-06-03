3d8f51e22453c9c5fe3cd90124dde411d4aac420
ashotbes <ashot.bes@gmail.com>
2023-06-04 00:06:52 +0400

    fixed scroll bg

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -37,11 +37,11 @@ const Navbar = () => {
                     <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
                         <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {setToggleMenu(false); setBodyOverflowScroll()}} />
                         <ul className="app__navbar-smallscreen_links">
-                            <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
-                            <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
-                            <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
-                            <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
-                            <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
+                            <li><a href="#home" onClick={() => {setToggleMenu(false); setBodyOverflowScroll()}}>Home</a></li>
+                            <li><a href="#about" onClick={() => {setToggleMenu(false); setBodyOverflowScroll()}}>About</a></li>
+                            <li><a href="#menu" onClick={() => {setToggleMenu(false); setBodyOverflowScroll()}}>Menu</a></li>
+                            <li><a href="#awards" onClick={() => {setToggleMenu(false); setBodyOverflowScroll()}}>Awards</a></li>
+                            <li><a href="#contact" onClick={() => {setToggleMenu(false); setBodyOverflowScroll()}}>Contact</a></li>
                         </ul>
                     </div>
                 )}
